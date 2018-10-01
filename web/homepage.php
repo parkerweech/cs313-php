ddb2120e79bcba43d39c149be3ec85b44d781142
Park_City <parkerweech.wei@gmail.com>
2018-09-30 22:14:54 -0600

    changed stuff

--- web/homepage.php
+++ web/homepage.php
@@ -18,7 +18,7 @@
 
 <div class="topnav">
   <a href="https://pure-badlands-14905.herokuapp.com/homepage.html">Homepage</a>
-  <img src="mountains_long.png">
+  <img src="mountains_long.jpg">
 </div>
 
 <div class="row">
