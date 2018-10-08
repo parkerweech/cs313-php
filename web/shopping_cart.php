56ccfd11d54bcfaa148000a8c81d5d769adde7dd
Park_City <parkerweech.wei@gmail.com>
2018-10-08 14:16:13 -0600

    added stuff

--- web/shopping_cart.php
+++ web/shopping_cart.php
@@ -5,7 +5,7 @@
 	<link rel="stylesheet" type="text/css" href="bootstrap.css">
 </head>
 <body>
-	<div class="container">
+	<div class="container-fluid">
 		<h1>This is the shopping cart page</h1>
 		<p>This is the paragraph.</p>
 		<p>This is another paragraph</p>
