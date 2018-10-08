9fbf9c8ad729598f09141da0fa2ca2bdd04a0fea
Park_City <parkerweech.wei@gmail.com>
2018-10-08 15:02:01 -0600

    added stuff

--- web/shopping_cart.php
+++ web/shopping_cart.php
@@ -19,13 +19,13 @@
 	<nav class="navbar navbar-default">
   		<div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
+      <a class="navbar-brand" href="#">Front Row</a>
     </div>
     <ul class="nav navbar-nav">
       <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Page 1</a></li>
-      <li><a href="#">Page 2</a></li>
-      <li><a href="#">Page 3</a></li>
+      <li><a href="#">Music</a></li>
+      <li><a href="#">Sports</a></li>
+      <li><a href="#">Events</a></li>
     </ul>
   </div>
 </nav>
