69a460ad30ec6b4329c679618bf21ed286519256
Park_City <parkerweech.wei@gmail.com>
2018-10-08 10:05:28 -0600

    added stuff

---
+++ web/shopping_cart.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Shopping Cart</title>
+	<link rel="stylesheet" type="text/css" href="bootsrap-grid.css">
+</head>
+<body>
+
+	<h1>This is the shopping cart page</h1>
+
+</body>
+</html>
