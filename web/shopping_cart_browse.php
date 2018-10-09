acdd48bd6fc37abfe351deb0e1ee316c86f054bf
Park_City <parkerweech.wei@gmail.com>
2018-10-08 20:49:17 -0600

    added stuff

--- web/shopping_cart_browse.php
+++ web/shopping_cart_browse.php
@@ -10,8 +10,41 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+  	<style>
+
+  body {
+      position: relative;
+      top: 20;
+      padding: 5px;
+  }
+
+  ul.nav-pills {
+      position: fixed;
+      top: 460px;
+      padding: 5px;
+  }
+  div.col-sm-9 div {
+      height: 250px;
+      font-size: 28px;
+  }
+
+  #Concerts {color: #fff; background-color: #1E88E5;}
+  #Festivals {color: #fff; background-color: #673ab7;}
+  #Football {color: #fff; background-color: #ff9800;}
+  #Basketball {color: #fff; background-color: #00bcd4;}
+  #Baseball {color: #fff; background-color: #009688;}
+  
+  @media screen and (max-width: 810px) {
+    #section1, #section2, #section3, #section41, #section42  {
+        margin-left: 150px;
+    }
+  }
+  </style>
+  
 </head>
-<body>
+
+<body data-spy="scroll" data-target="#myScrollspy" data-offset="20">
 <div class="jumbotron">
 		<h1> Front Row</h1>
 		<p> Concert, sports and event tickets</p>
