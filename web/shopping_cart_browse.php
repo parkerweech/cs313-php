8582538e015092d73b46603d0ee275e654073230
Park_City <parkerweech.wei@gmail.com>
2018-10-10 11:29:04 -0600

    added stuff

--- web/shopping_cart_browse.php
+++ web/shopping_cart_browse.php
@@ -83,7 +83,7 @@
         <img src="OBJcatch.jpg" alt="Giants" style="width:100%;">
         <div class="carousel-caption">
         	<h3>Giants vs. Cowboys</h3>
-        	<p>Click to add event to cart.</p>
+        	<p>12/20/18</p>
       	</div>
       	<button type="button" class="btn btn-primary" >Add to Cart</button>
       </div>
@@ -92,7 +92,7 @@
         <img src="mlb.jpg" alt="Baseball" style="width:100%;">
         <div class="carousel-caption">
         	<h3>Red Sox vs. Astros - Game 1</h3>
-        	<p>Click to add event to cart.</p>
+        	<p>11/12/18</p>
       	</div>
       	<button type="button" class="btn btn-primary">Add to Cart</button>
       </div>
@@ -101,7 +101,7 @@
         <img src="lifeis.jpg" alt="Beautiful" style="width:100%;">
         <div class="carousel-caption">
         	<h3>Life is Beautiful Festival</h3>
-        	<p>Click to add event to cart.</p>
+        	<p>9/21/19</p>
       	</div>
       	<button type="button" class="btn btn-primary">Add to Cart</button>
       </div>
@@ -110,7 +110,7 @@
         <img src="TOP.jpg" alt="Twenty" style="width:100%;">
         <div class="carousel-caption">
         	<h3>Twenty One Pilots - Bandito Tour</h3>
-        	<p>Click to add event to cart.</p>
+        	<p>Tour Dates Vary</p>
       	</div>
       	<button type="button" class="btn btn-primary">Add to Cart
       </div>
