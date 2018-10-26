d627f29df98582c5f422803436afcd121a8ce480
Park_City <parkerweech.wei@gmail.com>
2018-10-26 14:32:12 -0600

    blah

---
+++ web/cart.php
@@ -0,0 +1,25 @@
+<?php 
+
+$shows = $_POST["cart"];
+
+?>
+
+<html>
+	<head>
+		<title>Shopping Cart</title>
+	</head>
+
+	<body>
+
+		<?
+
+			foreach ($shows as $show) {
+				$show_clean = htmlspecialchars($show)
+				echo "<li><p>$show_clean</p></li>";
+			}
+
+		?>
+
+	</body>
+
+</html>
