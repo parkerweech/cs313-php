3ef53e1a688b8d5e07525928a97a8335f2f18994
Park_City <parkerweech.wei@gmail.com>
2018-11-15 10:39:22 -0700

    Added stuff

--- web/show.php
+++ web/show.php
@@ -10,7 +10,9 @@ try
   $dbUser = $dbOpts["user"];
   $dbPassword = $dbOpts["pass"];
   $dbName = ltrim($dbOpts["path"],'/');
+
   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $ex)
